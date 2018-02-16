143349334f57ae4d17954ac812eaa2382acd2552
haykam821 <haykam821@users.noreply.github.com>
2018-02-16 14:25:36 -0500

    Some working things

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 #!/usr/bin/env node
 const program = require("yargs");
 const semver = require("semver");
+const request = require("request-promise");
 
 program.command(["how [os]", "howto [os]"], "Checks how to jailbreak a version.", {}, checkJailbreak);
 program.command("exists [os]", "Checks if a version is jailbreakable.", {}, checkIfJailbreakable);
 program.help();
 program.argv;
 
-function checkJailbreak(args) {
-    console.log(args);
+async function checkJailbreak(args) {
+    let output = await request({
+        method: 'GET',
+        url: 'https://canijailbreak.com/jailbreaks.json',
+        json: true
+    });
+    let matches = output.jailbreaks.filter(function (value) {
+        return semver.satisfies(args.os, `${value.ios.start} - ${value.ios.end}`)
+    });
+
+    console.log(matches)
 };
-function checkIfJailbreakable(args) {
+async function checkIfJailbreakable(args) {
     console.log(args);
 };
