cb9fe901e5c7a11e0d67212219f625cd578706c7
haykam821 <haykam821@users.noreply.github.com>
2018-02-16 14:00:27 -0500

    Get started

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const yargs = require("yargs");
+#!/usr/bin/env node
+const program = require("yargs");
+
+program.command(["how [os]", "howto [os]"], "Checks how to jailbreak a version.", {}, checkJailbreak);
+program.command("exists [os]", "Checks if a version is jailbreakable.", {}, checkIfJailbreakable);
+program.help();
+program.argv;
+
+function checkJailbreak(args) {
+    console.log(args);
+};
+function checkIfJailbreakable(args) {
+    console.log(args);
+};
