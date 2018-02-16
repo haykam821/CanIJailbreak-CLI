212ed43e7f34a292249866bb0366db4eabc2bd60
haykam821 <haykam821@users.noreply.github.com>
2018-02-16 14:27:37 -0500

    Make everything async!

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 #!/usr/bin/env node
-const program = require("yargs");
-const semver = require("semver");
-const request = require("request-promise");
 
-program.command(["how [os]", "howto [os]"], "Checks how to jailbreak a version.", {}, checkJailbreak);
-program.command("exists [os]", "Checks if a version is jailbreakable.", {}, checkIfJailbreakable);
-program.help();
-program.argv;
+(async function() {
+    const program = require("yargs");
+    const semver = require("semver");
+    const request = require("request-promise");
 
-async function checkJailbreak(args) {
-    let output = await request({
-        method: 'GET',
-        url: 'https://canijailbreak.com/jailbreaks.json',
-        json: true
-    });
-    let matches = output.jailbreaks.filter(function (value) {
-        return semver.satisfies(args.os, `${value.ios.start} - ${value.ios.end}`)
-    });
+    program.command(["how [os]", "howto [os]"], "Checks how to jailbreak a version.", {}, checkJailbreak);
+    program.command("exists [os]", "Checks if a version is jailbreakable.", {}, checkIfJailbreakable);
+    program.help();
+    program.argv;
 
-    console.log(matches)
-};
-async function checkIfJailbreakable(args) {
-    console.log(args);
-};
+    async function checkJailbreak(args) {
+        let output = await request({
+            method: 'GET',
+            url: 'https://canijailbreak.com/jailbreaks.json',
+            json: true
+        });
+        let matches = output.jailbreaks.filter(function (value) {
+            return semver.satisfies(args.os, `${value.ios.start} - ${value.ios.end}`)
+        });
+
+        console.log(matches)
+    };
+    async function checkIfJailbreakable(args) {
+        console.log(args);
+    };
+})();
