735325f34417a84c025078f332f017fff3f3924e
haykam821 <haykam821@users.noreply.github.com>
2018-02-16 14:28:16 -0500

    Some changes and things

--- index.js
+++ index.js
@@ -5,22 +5,21 @@
     const semver = require("semver");
     const request = require("request-promise");
 
+    const output = await request({
+        method: 'GET',
+        url: 'https://canijailbreak.com/jailbreaks.json',
+        json: true
+    });
+
     program.command(["how [os]", "howto [os]"], "Checks how to jailbreak a version.", {}, checkJailbreak);
     program.command("exists [os]", "Checks if a version is jailbreakable.", {}, checkIfJailbreakable);
     program.help();
     program.argv;
 
     async function checkJailbreak(args) {
-        let output = await request({
-            method: 'GET',
-            url: 'https://canijailbreak.com/jailbreaks.json',
-            json: true
-        });
-        let matches = output.jailbreaks.filter(function (value) {
+        const matches = output.jailbreaks.filter(function (value) {
             return semver.satisfies(args.os, `${value.ios.start} - ${value.ios.end}`)
         });
-
-        console.log(matches)
     };
     async function checkIfJailbreakable(args) {
         console.log(args);
